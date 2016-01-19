f3224c6d42618b8e28e7803b3cd97e9d487041b9
Harminder Virk <virk.officials@gmail.com>
2016-01-19 18:33:11 +0530

    Initial commit

---
+++ app/Http/kernel.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const Middleware = use('Middleware')
+
+/*
+|--------------------------------------------------------------------------
+| Global Middleware
+|--------------------------------------------------------------------------
+|
+| Global middleware are executed on every request and must be defined
+| inside below array.
+|
+*/
+const globalMiddleware = [
+  'Adonis/Middleware/Cors',
+  'Adonis/Middleware/BodyParser',
+  'Adonis/Middleware/Flash',
+  'Adonis/Middleware/Shield'
+]
+
+/*
+|--------------------------------------------------------------------------
+| Named Middleware
+|--------------------------------------------------------------------------
+|
+| Named middleware are key/value pairs. Keys are later used on routes
+| which binds middleware to specific routes.
+|
+*/
+const namedMiddleware = {
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register Middleware
+|--------------------------------------------------------------------------
+|
+| Here we finally register our defined middleware to Middleware provider.
+|
+*/
+Middleware.global(globalMiddleware)
+Middleware.named(namedMiddleware)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Adonis App Development
+
+This is the development repository of Adonis application. It is created to have integeration tests for the final application.
